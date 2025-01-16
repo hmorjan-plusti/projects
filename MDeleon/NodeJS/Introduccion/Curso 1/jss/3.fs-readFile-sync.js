3f860b561823b1853ab51e678637c0297cc0b0c0
MarcoDeLeo91 <mdeleon.plusti@gmail.com>
2025-01-16 17:34:47 -0600

    Practicas de operacios sincronas y asincronas, promise, path, ls y process

---
+++ MDeleon/NodeJS/Introduccion/Curso 1/jss/3.fs-readFile-sync.js
@@ -0,0 +1,13 @@
+const fs = require('node:fs'); // readFileSync es una función sincrona
+
+console.log('leyendo el primer archivo');
+const text = fs.readFileSync('./prueba.txt', 'utf-8') 
+
+    console.log(text);
+
+console.log('hacer cosas mientras lee el archivo');
+
+console.log('leyendo el segundo archivo');
+const text2 = fs.readFileSync('./prueba2.txt', 'utf-8')
+ 
+    console.log(text2);
