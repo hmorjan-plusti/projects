4a05f1b6ee9db200b270f3fb224fded7606988d8
MarcoDeLeo91 <mdeleon.plusti@gmail.com>
2025-01-24 14:22:04 -0600

    clase3: Practica de Desarrollo de Api Rest

--- MDeleon/NodeJS/Introduccion/Curso 1/9.http.js
+++
@@ -1,8 +0,0 @@
-const http = require('http')
-const server = http.createServer((req, res) => {
-  res.end('Hola mundo')
-})
-
-server.listen(0, () => {
-  console.log(`server listen on port http://localhost:${server.address().port}`)
-})
